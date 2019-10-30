74aa9d016a8c4c20cc87daae8c1dae938667babb
Osovich <yokoroz@gmail.com>
2019-10-29 22:19:21 -0400

    recursive reduce

--- recursive-reduce.js
+++ recursive-reduce.js
@@ -1,3 +1,8 @@
-function reduce(arr, fn, initial){
-    
-}
+function reduce(arr, fn, initial) {
+    return (function one(index, value) {
+      if (index > arr.length - 1) return value
+      return one(index + 1, fn(value, arr[index], index, arr))
+    })(0, initial)
+  }
+
+  module.exports = reduce
